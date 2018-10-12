12bbc91e977601dacf31850a1db411f8049231d1
Sean White <seanwhite211@gmail.com>
2018-10-12 19:12:01 -0400

    add character, character animations and assets

---
+++ Assets/Character/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class Movement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    private Animator animator;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Run", z);
+        animator.SetFloat("Turn", x, 1f, Time.deltaTime * 5f);
+
+
+
+        transform.Translate(x *.03f, 0, z * .07f);
+    }
+}
