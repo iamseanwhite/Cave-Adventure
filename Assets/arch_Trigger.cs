dde67f8d0c04aac9eb2961963b23f8a2f0e3a12d
BNice30 <bsmi7333@gmail.com>
2018-10-29 17:51:39 -0500

    Cave update
    Added rock trap. Added water. Added wall. Added cliff. Added light at final area.

---
+++ Assets/arch_Trigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arch_Trigger : MonoBehaviour {
+    public GameObject[] rollers;
+    public GameObject hurt;
+    public float xSpeed=0;
+    public float ySpeed=0;
+    public float ballspeed;
+    // Use this for initialization
+    void Start() {
+       // rollers = GameObject.FindGameObjectsWithTag("Rocks");
+        //InvokeRepeating("Roll", 1, 1); 
+	}
+	
+    	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //xSpeed = xSpeed * .0002f;
+        ySpeed = ySpeed +1f;
+
+        Debug.Log(other.name + "Has Entered");
+
+        if (other.name != "Rock1")
+        {
+            if (other.name != "Rock2"){
+                rollers = GameObject.FindGameObjectsWithTag("Rock");
+                InvokeRepeating("Roll", 1, 1);
+            }
+        }
+        // myTransform.position = Vector3.up * Time.time;
+    }
+
+    void Roll()
+    {
+        Debug.Log("Roll");
+        GameObject rock = rollers[Random.Range(0, rollers.Length)];
+        Rigidbody body= rock.GetComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.None;
+        body.AddTorque(new Vector3(xSpeed, 0, ySpeed)* ballspeed*Time.deltaTime);
+        //rock.transform.position = Vector3.forward ;
+        //Instantiate(hurt,rock.transform.position,Quaternion.identity);
+    }
+}
