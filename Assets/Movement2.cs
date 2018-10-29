73b80dd25df37beb58738a7259a09955bc5ab04c
Sean White <seanwhite211@gmail.com>
2018-10-28 23:07:46 -0400

    add attack animation and footstep audio

--- Assets/Movement2.cs
+++ Assets/Movement2.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,11 @@ public class Movement2 : MonoBehaviour {
 
     private Animator animator;
     Rigidbody rigidBody;
+    AudioSource footstep;
+
+    float timeStart = 0;
+    float timeEnd = 0;
+    bool isAttacking = false;
     //Quaternion rotation;
 	
     // Use this for initialization
@@ -18,30 +24,59 @@ public class Movement2 : MonoBehaviour {
 
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody>();
+        footstep = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        //if (x > .1 || z > .1) animator.SetBool("Attack", false);
 
-        animator.SetFloat("Run", z, .1f, Time.deltaTime );
-        animator.SetFloat("Turn", x, 1f, Time.deltaTime );
-     
-        transform.rotation *= Quaternion.AngleAxis(rotateVelocity * x , Vector3.up);
         
+
+        if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Unarmed-Attack-L3"))
+        {
+            animator.SetBool("Attack", false);
+            x = Input.GetAxis("Horizontal");
+            z = Input.GetAxis("Vertical");
+
+            animator.SetFloat("Run", z, .05f, Time.deltaTime );
+            animator.SetFloat("Turn", x, 1f, Time.deltaTime * 5 );
+        
+        
+
+            transform.rotation *= Quaternion.AngleAxis(rotateVelocity * x , Vector3.up);
+        }
+
     }
 
     void FixedUpdate()
     {
-        Vector3 movement;
-        if (z > 0)
-            movement = transform.forward * 5 * z;
-        else
-            movement = transform.forward * 2 * z;
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("Attack", true);
+            isAttacking = true;
+        }
 
-        movement.y = rigidBody.velocity.y;
-        rigidBody.velocity = movement;
+        if (!this.animator.GetCurrentAnimatorStateInfo(0).IsName("Unarmed-Attack-L3"))
+        {
+            Vector3 movement;
+            if (z > 0) { 
+                movement = transform.forward * 5 * z;
+                //InvokeRepeating("PlayFootstep", .1f, 1.0f);
+            }
+            else
+                movement = transform.forward * 2 * z;
+        
+            movement.y = rigidBody.velocity.y;
+            rigidBody.velocity = movement;
+        }
+
+        
     }
+
+    //void PlayFootstep()
+    //{
+    //    footstep.Play();
+    //}
 }
