739ef1719f9586911fd2850a67af7651858b9a2c
Sean <sjw@red.one>
2018-11-23 22:40:39 -0500

    tiger ai update

--- Assets/Scripts/TiggerTriggers.cs
+++ Assets/Scripts/TiggerTriggers.cs
@@ -24,19 +24,40 @@ public class TiggerTriggers : MonoBehaviour {
         {
             animator.SetBool("IsClose", true);
 
-            if (Vector3.Distance(Melvin.transform.position, this.transform.position) > 2)
+            if (Vector3.Distance(Melvin.transform.position, this.transform.position) > 5)
             {
+                if (Vector3.Distance(Girl.transform.position, this.transform.position) > 3)
+                {
+                    animator.SetBool("TooFarToAttack", true);
+                    animator.SetBool("Attack1", false);
+                    //animator.SetBool("Walk", false);
+                }
+                else
+                {
+                    animator.SetBool("TooFarToAttack", false);                    
+                }
                 //start running towards girl               
                 var targetRotation = Quaternion.LookRotation(Girl.transform.position - transform.position);
-                tigerMovemement = transform.forward * 5;
+                //tigerMovemement = transform.forward * 3;
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
+               
             }
 
             else
             {
                 //run towards melvin
+                if (Vector3.Distance(Melvin.transform.position, this.transform.position) < 2)
+                {
+                    animator.SetBool("Attack1", true);
+                    animator.SetBool("TooFarToAttack", false);
+                }
+                else
+                { 
+                    animator.SetBool("Attack1", false);
+                    animator.SetBool("TooFarToAttack", true);                    
+                }
                 var targetRotation = Quaternion.LookRotation(Melvin.transform.position - transform.position);
-                tigerMovemement = transform.forward * 5;
+                //tigerMovemement = transform.forward * 3;
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
             }
 
@@ -45,11 +66,22 @@ public class TiggerTriggers : MonoBehaviour {
             rigidBody.velocity = tigerMovemement;
 
             //and when they meet, start attacking her
-            if (Vector3.Distance(Girl.transform.position, this.transform.position) < 3)
+            if (Vector3.Distance(Girl.transform.position, this.transform.position) < 2)
             {
+                //animator.SetBool("Walk", true);
                 Random.InitState(System.DateTime.Now.Millisecond);
                 int randomNumber = Random.Range(1, 2);
                 animator.SetBool("Attack" + randomNumber, true);
+                //animator.SetBool("TooFarToAttack", false);                
+            }
+
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("hit"))
+            {
+                tigerMovemement = transform.forward * .1f;
+            }
+            else
+            {
+                tigerMovemement = transform.forward * 5f;
             }
             
             
