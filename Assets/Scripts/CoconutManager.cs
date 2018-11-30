92e21c5c54a1002f17399ae18d5dcecbe9aac03b
jonathanpraleigh <jon_raleigh@hotmail.com>
2018-11-29 23:37:09 -0800

    Updates

---
+++ Assets/Scripts/CoconutManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoconutManager : MonoBehaviour
+{
+    Text Coconut_Counter;
+
+    void Awake()
+    {
+        Coconut_Counter = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        Coconut_Counter.text = MelvinManager.currentCoconutCount.ToString();
+    }
+}
