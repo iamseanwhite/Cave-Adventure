818614c54f987cbd55c160f263ed380859f1ce77
jonathanpraleigh <jon_raleigh@hotmail.com>
2018-11-29 21:43:50 -0800

    Updates

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject Melvin;
+    public GameObject GateGuard;
+
+    public void TriggerDialogue()
+    {
+        float dist = Vector3.Distance(Melvin.transform.position, GateGuard.transform.position);
+        if (dist < 20)
+        {
+            FindObjectOfType<DialogueManager>().StartDialog(dialogue);
+        }
+    }
+}
