d5be2e7991942479784c38d9e81e8ebc821efe7e
Sean White <seanwhite211@gmail.com>
2018-11-30 23:13:09 -0500

    scene updates

--- Assets/Scripts/PerspectiveScript.cs
+++ Assets/Scripts/PerspectiveScript.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PerspectiveScript : MonoBehaviour {
 
-    public GameObject mainCamera;
+    GameObject mainCamera;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+    void OnLevelWasLoaded()
+    {
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+        mainCamera = allObjects.FirstOrDefault(x => x.name.Equals("Camera"));
+    }
+
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(mainCamera.transform);
